803e0d3a43fb0de3bec6faedb3727147c7d76097
Eric Solinas <ericsolinas@hotmail.it>
2021-07-27 19:19:15 +0200

    seaborn plots after filtering
    filter outliers according to ranges of varref.tsv from https://github.com/ratschlab/HIRID-ICU-Benchmark/
    plot filtered datasets using seaborn and matplotlib
    sort notebook cells to give a more logical order

--- plots.R
+++ plots.R
@@ -1,11 +1,13 @@
 library(ggplot2)
 options(scipen = 999)
 
-for (f in list.files("./results/distribution_plots", pattern = "*.csv")) {
-  tmp <- read.csv(paste("./results/distribution_plots",
+# plot original distributions
+for (f in list.files("./results/distribution_plots/data")) {
+  tmp <- read.csv(paste("./results/distribution_plots/data",
     f,
     sep = "/"
   ))
+  print(f)
   ggplot(tmp, aes(y = value)) +
     geom_boxplot() +
     ggtitle(gsub(".csv", "", f)) +
@@ -40,43 +42,3 @@ for (f in list.files("./results/distribution_plots", pattern = "*.csv")) {
     height = 6
   )
 }
-
-# manual plotting
-f <- "Fluid balance in.csv"
-tmp <- read.csv(paste("./results/distribution_plots",
-  f,
-  sep = "/"
-))
-ggplot(tmp, aes(y = value)) +
-  geom_boxplot() +
-  ggtitle(gsub(".csv", "", f)) +
-  ylab("Value") +
-  theme_bw() +
-  theme(
-    axis.ticks.x = element_blank(),
-    axis.text.x = element_blank()
-  )
-ggsave(
-  paste(
-    "./results/distribution_plots",
-    gsub(".csv", " BOX.jpg", f),
-    sep = "/"
-  ),
-  width = 6,
-  height = 6,
-)
-ggplot(tmp, aes(x = value)) +
-  geom_histogram() +
-  ggtitle(gsub(".csv", "", f)) +
-  xlab("Value") +
-  ylab("Count") +
-  theme_bw()
-ggsave(
-  paste(
-    "./results/distribution_plots",
-    gsub(".csv", " HIST.jpg", f),
-    sep = "/"
-  ),
-  width = 6,
-  height = 6
-)
